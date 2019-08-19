fa13ae6c81324c41553a177d4647e652bee8dc58
wajid <wajid.javeed@appflox.xom>
2019-08-18 21:47:28 -0700

    change password validation

--- src/app/users/change-password/change-password.component.ts
+++ src/app/users/change-password/change-password.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { apiService } from 'src/app/services/api.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { catchError, retry } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { MustMatch } from 'src/app/common/must-match-validator';
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
@@ -16,14 +17,19 @@ export class ChangePasswordComponent implements OnInit {
   changepasswordForm:FormGroup
   errormessage: any;
   constructor(private apiservice:apiService,
-    private spinner:NgxSpinnerService) { }
+    private spinner:NgxSpinnerService,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.changepasswordForm=new FormGroup({
-      password:new FormControl('',[Validators.required]),
+    this.changepasswordForm=this.formBuilder.group({
+      password:new FormControl('',[Validators.required, Validators.minLength(8)]),
       password_confirmation:new FormControl('',[Validators.required]),
       token:new FormControl(this.token)
-    })
+    },
+    {
+      validator: MustMatch('password', 'password_confirmation')
+  }
+    )
   }
 
   updatePassword(val){
